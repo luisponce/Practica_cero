62db449b11cc730a88a9fc3d553889e0f5c031bb
luis M Ponce <luisponcekun@gmail.com>
2013-08-14 23:20:53 -0500

    Fixed bubble, now working (missing time output)

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,47 +1,34 @@
+
 #include <iostream>
 #include <string>
-#include <cstring>
 #include <vector>
 
 using namespace std;
 
-vector <string> arrInitial;  //Creacion del arreglo.
-
-int compare(string str1, string str2){  //Funcion para comparar dos strings.
-  char a[str1.length()];
-  for(int i=0; i<str1.length(); i++){
-    a[i]=str1[i];
-  }
-  char b[str2.length()];
-  for(int j=0;j<str1.length(); j++){
-    b[j]=str2[j];
-  }
-  return strcmp(a,b);
-}
+vector <string> arr;
 
 int main (){
   string temp; //Temporal para guardar los valores leidos.
-  while(cin>>temp){  //Lector de valores "mientras pueda".
-    arrInitial.push_back(temp);
+  while(cin>>temp){  //Lee valores "mientras pueda".
+    arr.push_back(temp);
   }
 
-  for (int i=0; i<arrInitial.size(); i++){  //Inicio del bubble sort.
-    bool compared = false;  //Booleano para terminar la ejecucion cuando no se compare.
-    for(int j=i+1; j>=arrInitial.size()-i; j++){
-      if(compare(arrInitial[j-1],arrInitial[j])>0){
-	temp = arrInitial[j-1];
-	arrInitial[j-1]=arrInitial[j];
-	arrInitial[j]=temp;
-	compared = true;
+  for (int i=0; i<arr.size(); i++){
+    for(int j=arr.size()-1; j>i; j--){
+      if(arr[j].compare(arr[j-1]) < 0){
+	//intercambiar arr[j] y arr[j-1]
+	temp = arr[j-1];
+	arr[j-1] = arr[j];
+	arr[j] = temp;
       }
     }
-
-    //if(!compared) break;  //Break para parar el codigo cuando no se compare.
   }
-
-  cout<<"Arreglo Ordenado: "<<endl;  //Impresion del arreglo.
-  for(int i=0;i<arrInitial.size(); i++){  //Ciclo para impresion del arreglo.
-    cout<<arrInitial[i]<<endl;
+  
+  //imprimir arreglo ordenado
+  cout<<"Arreglo Ordenado: "<<endl;
+  for(int i=0; i<arr.size(); i++){
+    cout<<arr[i]<<endl;
   }
+    
   return 0;
 }
